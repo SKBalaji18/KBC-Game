2ef646b4ba0d723de7f5752f45bff621a743fd07
Balaji S.K <skbalajiskb@gmail.com>
2024-10-08 20:05:41 +0530

    player changes

--- src/Components/PlayerScreen/index.js
+++ src/Components/PlayerScreen/index.js
@@ -1,35 +1,62 @@
 import React, { useState } from 'react';
-import { useNavigate } from "react-router-dom";
 import {useGame} from '../../Context/GameProvider'
-import './index.css'
 
 const PlayerScreen = () => {
-    const {playerJoin} = useGame();
-    const navigate = useNavigate();
+
+    const { currentQuestion, handleAnswerSubmit,playerJoin ,congratsMessage,feedbackMessage} = useGame();
+    const [selectedAnswer, setSelectedAnswer] = useState("");
     const [playerName, setPlayerName] = useState("");
+
+    const [showQuestion,setShowQuestion] = useState(false)
 
     const handleJoin = () => {
         if (playerName) {
             playerJoin(playerName);
+            setShowQuestion(true)
             setPlayerName(""); // Clear input after joining
-            navigate('/')
         }
     };
 
+    const handleOptionChange = (event) => {
+      setSelectedAnswer(event.target.id);
+    };
+
     return (
-      <div className='input-container'>
-        <input 
-            type="text" 
-            value={playerName} 
-            className='user-input-el'
-            onChange={(e) => setPlayerName(e.target.value)} 
-            placeholder="Enter your name" 
-        />
-        <button onClick={handleJoin} className="button">Join</button>
-      </div>
-  );
+        <div className='input-container'>
+          {showQuestion?(<>
+            <h2>{currentQuestion.question}</h2>
+            <div className="options">
+                {Object.entries(currentQuestion.options).map(([key, value]) => (
+                    <div key={key} className="option">
+                      <input
+                          type="radio"
+                          id={key}
+                          name="answer"
+                          value={selectedAnswer}
+                          checked={selectedAnswer === key}
+                          onChange={handleOptionChange}
+                      />
+                      <label htmlFor={key}>{value}</label>
+                    </div>
+                ))}
+            </div>
+            <button className="button" onClick={() => handleAnswerSubmit(selectedAnswer)}>Submit Answer</button>
+            {congratsMessage && <p style={{ color: 'green' }}>{congratsMessage}</p>}
+            {feedbackMessage && <p style={{ color: 'red' }}>{feedbackMessage}</p>}
+            </> ):(
+          <>
+            <input 
+                type="text" 
+                value={playerName} 
+                className='user-input-el'
+                onChange={(e) => setPlayerName(e.target.value)} 
+                placeholder="Enter your name" 
+            />
+            <button onClick={handleJoin} className="button">Join</button>
+          </>
+        )}
+        </div>
+    );
 };
 
 export default PlayerScreen;
-
-
